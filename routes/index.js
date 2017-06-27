2b232bba585e6a6852c6734d792e7cff8a02590a
Sarthak <sarthakn18@gmail.com>
2017-06-27 21:34:08 +0530

    Added movies_single and notFound routes

--- routes/index.js
+++ routes/index.js
@@ -21,12 +21,42 @@ exports.home = function(req,res){
 
 
 exports.movie_single = function(req,res){
+
+
+  var movies = moviesJSON.movies;
+
   var episode_number = req.params.episode_number;
-  res.send('This is the page for episode '+episode_number);
+
+  if (episode_number >= 1 && episode_number<=6){
+    var movie = movies[episode_number - 1];
+
+    var title = movie.title;
+
+    var main_characters = movie.main_characters;
+
+    res.render('movies_single',{
+      movies: movies,
+      title: title,
+      movie: movie,
+      main_characters: main_characters
+
+    });
+  }
+  else{
+    res.render('notFound',{
+      movies: movies,
+      title: 'This is not the page you are looking for'
+    });
+  }
+
 };
 
 //Not found
 
 exports.notFound = function(req,res){
-  res.send('This is not the page you are looking for');
+  var movies = moviesJSON.movies;
+  res.render('notFound',{
+    movies: movies,
+    title: 'This is not the page you are looking for'
+  });
 };
